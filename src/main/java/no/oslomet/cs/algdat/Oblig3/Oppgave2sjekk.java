38560130c4745756fb26f57783414f8915072017
MortenBarhaugen <42533141+MortenBarhaugen@users.noreply.github.com>
2022-10-24 09:37:34 +0200

    test av gradle

--- src/main/java/no/oslomet/cs/algdat/Oblig3/Oppgave2sjekk.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig3/Oppgave2sjekk.java
@@ -7,6 +7,7 @@ public class Oppgave2sjekk {
         /*Integer[] a = {4,7,2,9,4,10,8,7,4,6};
         SBinTre<Integer> tre = new SBinTre<>(Comparator.naturalOrder());
         for (int verdi : a) { tre.leggInn(verdi); }*/
+        //test
 
         Integer[] b = {4,3,2,1};
         SBinTre<Integer> tre2 = new SBinTre<>(Comparator.naturalOrder());
